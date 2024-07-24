e8f6c53db9ecd7a9c369fb995a8c568bc32709aa
hyeon419 <hyeon419@shindo.hs.kr>
2024-07-24 11:55:41 +0900

    app

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 import streamlit as st
-import random as r
+import dice
+import updown
+import df
 
-st.header('주사위 게임', divider='rainbow')
-clicked = st.button('주사위던지기', type='primary')
+st.sidebar.title("메뉴선택")
+page = st.sidebar.radio("", ["주사위던지기", "업다운게임", "데이터분석"])
 
-if clicked :
-    n = r.randint(1,6)
-    st.image(f'./img/{n}.png')
+if page == "주사위던지기":
+  st.title("주사위던지기")
+  dice.show()
+
+elif page == "업다운게임":
+  st.title("업다운게임")
+  updown.show()
+
+elif page == "데이터분석":
+  st.title("데이터분석")
+  df.show()
+
+  
